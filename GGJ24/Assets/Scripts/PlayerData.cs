7fd7e21b1666106db887aad8920ee5a4878b3093
Yonghao Chen <raidenchen@hotmail.com>
2024-01-27 14:57:46 -0600

    Add PlayerData class.

---
+++ GGJ24/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    private List<(int, int)> miniGameGrade;
+
+    private int msgReceivedCount;
+
+    private List<(int, string)> answerList;
+
+    private int totalPlayerNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        msgReceivedCount = 1;
+    }
+
+    public void updateTotalPlayerNumber(int n)
+    {
+        totalPlayerNumber = n;
+    }
+
+    public void addAnswer(int n, string ans)
+    {
+        answerList.Add((n, ans));
+        msgReceivedCount += 1;
+    }
+
+    public void addGrade(int n, int grade)
+    {
+        miniGameGrade.Add((n, grade));
+    }
+}
