c11fb1314269cde5205696c356b760439ba44a7a
azeTrom17 <nmort17@aol.com>
2024-01-27 12:18:49 -0600

    added lobby connect code

---
+++ GGJ24/Assets/Scripts/Multiplayer/PlayerSetup.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] private TMP_InputField usernameField;
+
+    private readonly List<ulong> playerIDs = new();
+    private readonly List<string> playerUsernames = new();
+
+    private readonly int minimumPlayers = 2;
+
+    public override void OnNetworkSpawn()
+    {
+        ConnectPlayerServerRpc(NetworkManager.Singleton.LocalClientId, usernameField.text);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ConnectPlayerServerRpc(ulong clientId, string playerUsername)
+    {
+        // Wait until all players have loaded into the scene
+        playerIDs.Add(clientId);
+        playerUsernames.Add(playerUsername);
+
+        // Display connected players
+
+        if (playerIDs.Count < minimumPlayers)
+            return;
+
+        // Allow host to start game if enough players are ready
+    }
+
+    [ClientRpc]
+    private void StartGameClientRpc()
+    {
+        // Called by the Host, run on all Clients
+
+        // Start game
+    }
+}
