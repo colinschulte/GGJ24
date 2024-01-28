91d9e7d300a6e964d7e265f68e02aa62d5161639
azeTrom17 <nmort17@aol.com>
2024-01-28 00:05:26 -0600

    added title screen fade, added MenuBoxes to all screens

--- GGJ24/Assets/Scripts/GameManager.cs
+++ GGJ24/Assets/Scripts/GameManager.cs
@@ -1,13 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Image titleScreen;
     [SerializeField] private GameObject lobbyConnectScreen;
     [SerializeField] private GameObject promptScreen;
 
-    public void StartGame()
+    private int fade;
+
+    private void Start()
+    {
+        StartCoroutine(TitleScreenFade());
+    }
+    private void Update()
+    {
+        if (fade != 0)
+        {
+            Color color = titleScreen.color;
+            color.a += fade * Time.deltaTime;
+            titleScreen.color = color;
+        }
+    }
+    private IEnumerator TitleScreenFade()
+    {
+        fade = 1;
+
+        yield return new WaitForSeconds(2);
+
+        fade = 0;
+        
+        yield return new WaitForSeconds(1);
+
+        fade = -1;
+
+        yield return new WaitForSeconds(2);
+
+        fade = 0;
+
+        titleScreen.gameObject.SetActive(false);
+        lobbyConnectScreen.SetActive(true);
+    }
+
+
+    public void SelectStartGame()
     {
         lobbyConnectScreen.SetActive(false);
 
